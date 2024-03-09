9dfa1684d6e9bad63f874f611e3cbd5c2e3da6df
Alejandro9908 <joc.alejandro99@gmail.com>
2024-03-09 02:03:18 -0600

    rutas

--- routes/web.php
+++ routes/web.php
@@ -14,9 +14,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
+/*Route::get('/', function () {
     return view('welcome');
 });
 
 Route::get('/tipo-cambio/{fechaInicio}/{fechaFin}', [TipoCambioController::class, 'obtenerTipoCambio']);
-Route::get('/variables-disponibles', [TipoCambioController::class, 'obtenerVariablesDisponibles']);
+Route::get('/variables-disponibles', [TipoCambioController::class, 'obtenerVariablesDisponibles']);*/
